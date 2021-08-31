2dbdeb706966f6fcae4c08fce018d090b1134cfa
Yasir Hassan <hasanyasir01@gmail.com>
2021-08-31 15:30:17 -0700

    setup

---
+++ project/src/App.jsx
@@ -0,0 +1,9 @@
+import { GlobalStyle } from "./common/GlobalStyle";
+export const App = () => {
+    return (
+        <>
+            <GlobalStyle />
+            <h1>Hello App</h1>
+        </>
+    );
+};
